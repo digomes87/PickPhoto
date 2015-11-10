cd7d3ff494ef37617847d6d20d70d9ff79ba69e2
Nylon <diego.gomes87@gmail.com>
2015-11-10 18:51:56 -0200

    Praia e fumo HAHAHAHAHA

--- PickPhoto/ViewController.swift
+++ PickPhoto/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var infoLabel: UILabel!
 
+    var arrayPhoto = ["Loira","Ruiva","Morena","Mulata","Estranha Mas gata"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int{
+    
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        
+        return arrayPhoto.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return arrayPhoto[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        
+        let photoSelected = arrayPhoto[row]
+        
+        switch (photoSelected){
+        
+        case "Ruiva":
+            imageView.image = UIImage(named: "ruiva.jpg")
+            infoLabel.text = "Casava"
+            
+            
+        case "Loira":
+            imageView.image = UIImage(named: "loira.jpg")
+            infoLabel.text = "Me levaria facil pra cama"
+         
+        case "Morena":
+            imageView.image = UIImage(named: "morena.jpg")
+            infoLabel.text = "Deusulivre"
+            
+        case "Mulata":
+            imageView.image = UIImage(named: "mulata.jpg")
+            infoLabel.text = "Vem ni mim!"
+            
+        case "Estranha Mas gata":
+            imageView.image = UIImage(named: "estranha.jpeg")
+            infoLabel.text = "Praia e fumo!"
+            
+            
+        default:
+            imageView.image = UIImage(named: "temosParaTodosOsGostos.jpg")
+        }
+    }
 }
 
